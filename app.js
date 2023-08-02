bb64d2438d1e71d380485da0929eef30854ee089
barboza gonzalo <barbozagonzaloagustin8@gmail.com>
2023-08-02 15:22:48 -0300

    Se importaron las dependencias, se implementarion los middlewares

--- app.js
+++ app.js
@@ -10,10 +10,26 @@ require("dotenv").config();
 // require ejs
 require("ejs");
 
+// Db connection
+const { conexionDB } = require("./database");
+conexionDB();
+
+
 const app = express();
+
+//puerto
+const port = process.env.PORT || 4000;
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+// express: datos en formato json
+app.use(express.json());
+//motor de vistas
+app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -22,5 +38,9 @@ app.use(require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
+app.use((req, res, next)=>{
+    return res.status(404).render("404");
+})
+
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`Server on http://localhost:${port}`));
